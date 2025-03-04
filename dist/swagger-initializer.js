d1e597a8c772de76e8efb235143db9e96340919c
flowerinthenight <root@flowerinthenight.com>
2025-03-04 18:16:57 +0900

    point to actual docs

--- dist/swagger-initializer.js
+++ dist/swagger-initializer.js
@@ -3,8 +3,9 @@ window.onload = function() {
 
   // the following lines will be replaced by docker/configurator, when it runs in a docker-container
   window.ui = SwaggerUIBundle({
-    url: "swagger.yaml",
+    url: "apidocs.json",
     dom_id: '#swagger-ui',
+    defaultModelsExpandDepth: -1,
     deepLinking: true,
     presets: [
       SwaggerUIBundle.presets.apis,
